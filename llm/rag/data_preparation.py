a4aa013abc551a48853b742e46ef9a255bf7c00a
Zhao-HZ <18050086866@qq.com>
2026-01-20 23:45:10 +0800

    first upload

---
+++ llm/rag/data_preparation.py
@@ -0,0 +1,19 @@
+import pymilvus
+import pdfplumber
+from PIL import Image
+
+class DataPreparationPDF:
+    def __init__(self):
+        ...
+
+    def processPDF(self, pdf_path):
+        docs = []
+        with pdfplumber.open(pdf_path) as pdf:
+            for page_id, page in enumerate(pdf.pages, 1):
+                text = page.extract_text()        
+                print(text)
+
+        
+if __name__ == '__main__':
+    preparation = DataPreparationPDF()
+    preparation.processPDF('./llm/rag/res/Review - Recent Advances in Sensor Fusion Monitoring and Control Strategies in Laser Powder Bed Fusion A Review.pdf')
