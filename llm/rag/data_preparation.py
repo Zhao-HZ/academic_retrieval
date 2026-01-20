56402979cf91508ced7fcaa035ff0935044f5f3f
Zhao-HZ <18050086866@qq.com>
2026-01-20 23:47:01 +0800

    first upload

--- llm/rag/data_preparation.py
+++ llm/rag/data_preparation.py
@@ -6,12 +6,18 @@ class DataPreparationPDF:
     def __init__(self):
         ...
 
+    def getImageCaption(self):
+        
+
     def processPDF(self, pdf_path):
         docs = []
         with pdfplumber.open(pdf_path) as pdf:
             for page_id, page in enumerate(pdf.pages, 1):
                 text = page.extract_text()        
                 print(text)
+
+                for img in page.images:
+                    
 
         
 if __name__ == '__main__':
