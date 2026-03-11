20d9b9f8a14753c421adf8423064ae95ac5af8a5
ZHZ <18050086866@qq.com>
2026-03-11 22:05:07 +0800

    Update index_optimization.py
    Refactor retrieval classes to use a common database initialization method and improve query handling.

--- llm/rag/index_optimization.py
+++ llm/rag/index_optimization.py
@@ -1,16 +1,22 @@
-from pymilvus import MilvusClient
+from pymilvus import \
+    MilvusClient, AnnSearchRequest, Function, FunctionType
 from typing import Tuple, Iterator
+from embedding_model import get_bge_embeddings_single, BGE_EMBEDDING_DIM
 from rag_config import *
 
-class Retrieval:
+def initialize_db():
+    client = MilvusClient(
+        url=MILVUS_URI,
+        token=MILVUS_TOKEN
+    )
+    client.use_database(DB_NAME)
+    return client
+
+class FullTextRetrieval:
     def __init__(self):
-        self.client = MilvusClient(
-           ur=MILVUS_URI,
-           token=MILVUS_TOKEN
-        )  
-        self.client.use_database(DB_NAME)
+        self.client = initialize_db() 
     
-    def fullTextSearch(self, queries) -> list:
+    def query(self, queries) -> list:
         results = self.client.search(
             collection_name=COLLECTION_NAME,
             data=queries,
@@ -23,13 +29,56 @@ class Retrieval:
             # yield result['distance'], result['entity']['content'] 
         for i, result in enumerate(sparse_results):
             print(f"{i+1}. Score: {result['distance']:.4f}, Content: {result['entity']['content']}")
-        return list(map(lambda result: result['entity']['content'], sparse_results))
-  
-    def denseSearch(self, )
-   
-    def hybridSearch(self):
-        ...
-    
+        return list(map(lambda result: result['entity']['content'], sparse_results))  
+
+class HybridTextRetrieval:
+    def __init__(self):
+        self.client = initialize_db()
+        self.ranker = Function(
+            name="rrf",
+            input_field_names=[],
+            function_type=FunctionType.RERANK,
+            params={
+                'reranker': 'rrf',
+                "k": 100
+            }
+        )         
+
+    def query(self, query_text) -> list:
+        query_dense_vector = get_bge_embeddings_single(query_text)
+        search_param_1 = {
+            'data': [query_dense_vector],
+            'anns_field': "dense_vector",
+            'param': {'nprobe': 10},
+            'limit': 2
+        }
+        request1 = AnnSearchRequest(**search_param_1)
+
+        search_param_2 = {
+            'data': [query_text],
+            'anns_field': "sparse_vector",
+            'param': {'nprobe': 10},
+            'limit': 2
+        }
+        request2 = AnnSearchRequest(**search_param_2)
+
+        reqs = [request1, request2]
+
+        res = self.client.hybrid_search(
+            collection_name=COLLECTION_NAME,
+            reqs=reqs,
+            ranker=self.ranker,
+            limit=2,
+            output_fields=['content', 'metadata']
+        )
+
+        for hits in res:
+            print('TopK results:')
+            for hit in hits:
+                print(hit)
+
 if __name__ == '__main__':
-    ret = Retrieval()
-    ret.fullTextSearch(['graph neural-network'])
+    # ret = FullTextRetrieval()
+    ret = HybridTextRetrieval() 
+    ret.query(['graph neural-network'])
+    # ret.query('Graph')
